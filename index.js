bb54ad9872bf7d08c64530183bed3512b000829b
jemiloii <jemiloii@gmail.com>
2019-04-10 03:10:07 -0500

    Add logging and discord

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
+const characters = require('./lib/json/characters');
 const text2png = require('text2png');
 const Chess = require('chess.js').Chess;
 const chess = new Chess();
-
-const characters = {
-    'kww': '\uE040',
-    'qww': '\uE041',
-    'rww': '\uE042',
-    'bww': '\uE043',
-    'nww': '\uE044',
-    'pww': '\uE045',
-    'kbw': '\uE046',
-    'qbw': '\uE047',
-    'rbw': '\uE048',
-    'bbw': '\uE049',
-    'nbw': '\uE04A',
-    'pbw': '\uE04B',
-    'w'  : '\uE04C',
-    'kwb': '\uE050',
-    'qwb': '\uE051',
-    'rwb': '\uE052',
-    'bwb': '\uE053',
-    'nwb': '\uE054',
-    'pwb': '\uE055',
-    'kbb': '\uE056',
-    'qbb': '\uE057',
-    'rbb': '\uE058',
-    'bbb': '\uE059',
-    'nbb': '\uE05A',
-    'pbb': '\uE05B',
-    'b'  : '\uE05C'
-};
 
 const getChar = ({type, color, tile}) => type && color ? characters[`${type}${color}${tile}`] : characters[tile];
 
