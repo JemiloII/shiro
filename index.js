c1097af3a18d8f6ffa1aa1c99092d8749ac991b6
jemiloii <jemiloii@gmail.com>
2019-04-10 19:14:55 -0500

    wip fonts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const characters = require('./lib/json/characters');
+const legend = require('./lib/json/legend');
 const text2png = require('text2png');
 const Chess = require('chess.js').Chess;
 const chess = new Chess();
@@ -8,18 +9,20 @@ const getChar = ({type, color, tile}) => type && color ? characters[`${type}${co
 const tileColor = (ri, ci) => (ri + ci) % 2 === 0 ? 'w' : 'b';
 const tileBoard = (r, ri) => r.map((c, ci) => ({...c, tile: tileColor(ri, ci)}));
 const charBoard = r => r.map(c => getChar(c));
+const addLegend = (r, ri) => r.concat(legend.rows[ri]);
 const tiledBoard = chess.board().map(tileBoard);
-const characterBoard = tiledBoard.map(charBoard);
-
+const characterBoard = tiledBoard.map(charBoard).map(addLegend).concat(legend.cols);
 const boardToString = board => board.map(r => r.join('')).join('\n');
+console.log(characterBoard);
+console.log(boardToString(characterBoard));
 
 const image = text2png(boardToString(characterBoard), {
-    font: '50px Chess',
+    font: '50px Shiro',
     lineSpacing: 0,
     padding: 0,
     bgColor: 'white',
-    localFontPath: './assets/chess.otf',
-    localFontName: 'Chess',
+    localFontPath: './assets/shiro1.otf',
+    localFontName: 'Shiro',
     output: 'dataURL'
 });
 
