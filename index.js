1860b74fd2625874727114b4a9c7eecafa7aaa93
jemiloii <jemiloii@gmail.com>
2019-04-11 15:59:13 -0500

    Generates the board with the legend

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const Chess = require('chess.js').Chess;
 const chess = new Chess();
 const Jimp = require('jimp');
 
-const fontSize = 62.5;
+chess.move('e4');
+
 const text2pngOpts = {
-    font: `${fontSize}px Shiro`,
+    font: '62.5px Shiro',
     lineSpacing: 0,
     padding: 0,
     bgColor: 'white',
@@ -15,28 +16,27 @@ const text2pngOpts = {
     localFontName: 'Shiro',
     output: 'buffer'
 };
+
 const getChar = ({type, color, tile}) => type && color ? characters[`${type}${color}${tile}`] : characters[tile];
 
 const tileColor = (ri, ci) => (ri + ci) % 2 === 0 ? 'w' : 'b';
 const tileBoard = (r, ri) => r.map((c, ci) => ({...c, tile: tileColor(ri, ci)}));
 const charBoard = r => r.map(c => getChar(c));
-// const addLegend = (r, ri) => r.concat(legend.rows[ri]);
 const tiledBoard = chess.board().map(tileBoard);
-const characterBoard = tiledBoard.map(charBoard); //.map(addLegend).concat(legend.cols);
+const characterBoard = tiledBoard.map(charBoard);
 const boardToString = board => board.map(r => r.join('')).join('\n');
 console.log(characterBoard);
 const text = boardToString(characterBoard);
 
 const chessBoard = text2png(text, text2pngOpts);
 
-console.log(chessBoard);
+const generate = async (buffer) => {
+    const base = await Jimp.read('./assets/base.png');
+    const board = await Jimp.read(buffer);
 
-const dimension = fontSize * 9;
-new Jimp(dimension, dimension, async (error, image) => {
-    try {
-        const board = await Jimp.read(chessBoard);
-        console.log(await board.getBase64Async('image/png'));
-    } catch (e) {
-        console.log('Caught Jimp Error:', e);
-    }
-});
+    base.blit(board, 0, 0);
+    return await base.getBase64Async('image/png');
+};
+
+generate(chessBoard)
+    .then(console.log);
